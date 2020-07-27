44fe6e11f9d0b6145efe7f539dd1f7d49e24a4ef
Muhannad Alrisi <muhannad@local>
2020-07-27 15:32:29 +0400

    fetch the result from Sabre API and init the call using AJAX

---
+++ script.js
@@ -0,0 +1,109 @@
+let hotelButton = document.querySelector("#getHotelBtn");
+let responseBlock =  document.querySelector('#response_block'); 
+let hotelListDOM = document.querySelector('.hotelList'); 
+
+
+/**
+ * 
+ * @param {array} $data hotel object of rhe response 
+ *                 
+ */
+function display_result($data) { 
+    // create a DOM 
+    $data  = $data || {}; 
+
+    //Hide the block first  
+    responseBlock.classList.remove('d-none');
+
+    let hotelList  = fetch_hotel_list(localStorage.getItem('result')); 
+  
+    console.log(`Total hotel list is ${hotelList.length}`); 
+
+    trace = hotelList[0].HotelInfo; 
+    hotelList.map( hotel => {
+        _prepare_list(hotel.HotelInfo);  
+    }); 
+
+    let content  = responsesDOM.map((el) => el.innerHTML); 
+
+    responseBlock.innerHTML =content.join(' '); 
+}
+
+
+
+let responsesDOM = []; 
+
+function _prepare_list($data) { 
+    let $el = hotelListDOM.cloneNode(true); 
+    $el.querySelector('#hotel_title').innerHTML = $data.HotelName; 
+    $el.querySelector("#sabre_rate").innerHTML = 'Rate ' +$data.SabreRating; 
+    $el.querySelector("#address_line").innerHTML = $data.LocationInfo.Address.AddressLine1;
+
+    $el.querySelector('#hotel_logo').setAttribute('src', $data.Logo) ; 
+    let origin  = [
+        $data.LocationInfo.Address.CityName.value, 
+        $data.LocationInfo.Address.CountryName.value
+    ]; 
+
+    $el.querySelector("#origin").innerHTML = origin.join(' , '); 
+
+    
+    
+    let listItem;
+
+    for( contact in $data.LocationInfo.Contact) {
+        listItem = document.createElement('li');  
+        listItem.innerHTML = `${contact}: ${$data.LocationInfo.Contact[contact]}`; 
+        $el.querySelector('#contact_info').append(listItem);   
+    }
+    responsesDOM.push($el); 
+}
+
+
+/**
+ * 
+ * @param {string} $data encoded JSON string 
+ * @return {array} hotel list
+ */
+function fetch_hotel_list($data){ 
+    $data = $data || {}; 
+    //we assume that all the inputs are 
+    // JSON encoded string  
+    let data  = JSON.parse($data); 
+    return data.GetHotelAvailRS.HotelAvailInfos.HotelAvailInfo; 
+}
+
+async function get_hotel_list() {
+
+    await axios({
+      url: "./ajax.php",
+      data: {
+        hotel: "getHotel",
+      },
+      method: "POST",
+    })
+      .then((response) => {
+        localStorage.setItem("result", response.data);
+        display_result(response.data); 
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    axios({
+      url: "./ajax.php",
+      data: {
+        hotel: "getHotel",
+      },
+      method: "POST",
+    })
+      .then((response) => {
+        localStorage.setItem("result", response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+
+}
+
+hotelButton.addEventListener("click", get_hotel_list);
--- template.php
+++ template.php
@@ -1,55 +1,97 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Sabre Session Management</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
-</head>
-<body>
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
+      integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I"
+      crossorigin="anonymous"
+    />
 
+    <style type="text/css">
+    .hotel-block{ 
+
+      background:  #f8f9fa;
+
+      border-radius: 4px;
+      padding: 9px; 
+      box-sizing: border-box;
+
+    }
+    </style> 
+
+  </head>
+  <body>
     <div class="container">
-        <h1>
-            Session Management
-        </h1>
-        <h3>Sample of request </h3>
-        <hr />
-        <div class="overflow-auto bg-light mb-2" style="height: 300px;">
-            <code>
-              <pre>  
+      <h1>
+        Session Management
+      </h1>
+      <h3>Sample of request</h3>
+      <hr />
+      <div class="overflow-auto bg-light mb-2" style="height: 300px;">
+        <code>
+          <pre>  
                     <?php print_r($request_json); ?> 
-              </pre>
-            </code>
+        </pre>  
+        </code>
+      </div>
+
+      <button type="button" class="btn btn-primary btn-large" id="getHotelBtn">
+        Get the response
+      </button>
+
+      <div id="response_block" class="d-none" >
+        <div class="hotelList">
+        <div class="row  my-2 rounded  shadow-sm hotel-block">
+          <div class="col-12">
+            <div class="row">
+              <div class="col-9">
+                <h2 id="hotel_title">Holiday Inn</h2>
+              </div>
+              <div class="col-3">
+                <span id="sabre_rate">Rate</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-12">
+            <div class="row">
+              <div class="col-4">
+                  <img src="" alt="LOGO" id="hotel_logo" />
+              </div>
+              <div class="col-4">
+                <p class="mb-1">
+                  <span id="address_line">address_line</span>
+                  <span class="d-block text-muted" id="origin">Muscat , oman</span>
+                </p>
+                <ul class="mt-0 mb-0 text-muted font-italic" id="contact_info">
+                </ul>
+              </div>
+              <div class="col-4">2
+                  display map
+              </div>
+            </div>
+          </div>
         </div>
-
-
-        <a class="btn btn-primary btn-large" href="index.php?response=do">get the response  </a>
-
-        <?php if($_GET['response'] ?? false): ?> 
-        <h3>Sample of Response </h3>
-        <hr />
-        <div class="overflow-auto bg-light" style="height: 300px;">
-            <code>
-              <pre>  
-                    <?php print_r(json_decode($request->getBody()->getContents(),true)); ?> 
-              </pre>
-            </code>
         </div>
-        <?php endif; ?> 
-
-        
+      </div>
     </div>
 
-    <script>
-        var conn = new WebSocket('ws://localhost:8080');
-        conn.onopen = function(e) {
-            // console.log("Connection established!");
-            console.log(e);
-        };
+    <!-- javascript -->
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <script src="script.js"></script>
+    <script type="text/javascript">
+      var conn = new WebSocket("ws://localhost:8080");
+      conn.onopen = function (e) {
+        // console.log("Connection established!");
+        console.log(e);
+      };
 
-        conn.onmessage = function(e) {
-            console.log(e.data);
-        };
+      conn.onmessage = function (e) {
+        console.log(e.data);
+      };
     </script>
-</body>
+  </body>
 </html>
