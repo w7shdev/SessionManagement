71e9118998f1b56f279f0dff2c244c1b9f012801
Muhannad Alrisi <muhannad@local>
2020-07-30 11:34:56 +0400

    get the token session and use it in the web sockets class

--- src/SabreSession.php
+++ src/SabreSession.php
@@ -2,14 +2,37 @@
 namespace SessionSync;
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
+use SessionSync\SabreRestAPI; 
+
 ## TODO check the auto loading PSR-4 
-require_once (dirname(__DIR__) .'../vendor/autoload.php'); 
+require_once(dirname(__DIR__) .'../vendor/autoload.php'); 
+
 
 class SabreSession implements MessageComponentInterface {
     protected $clients;
 
+    /**
+     * Tokens
+     */
+    protected static $tokens = []; 
+
+    protected static $token_counter = 0; 
+    /**
+     * Sabre Object handler 
+     */
+    private $sabreApi; 
+
     public function __construct() {
         $this->clients = new \SplObjectStorage;
+
+        if(count(self::$tokens) < 1 ){
+            self::$token_counter++; 
+            $this->sabreApi = new SabreRestAPI(); 
+            self::$tokens[] = [
+                'token_access' => $this->sabreApi->get_token(), 
+                'count' =>  self::$token_counter
+            ]; 
+        }
     }
 
     public function onOpen(ConnectionInterface $conn) {
@@ -17,6 +40,8 @@ class SabreSession implements MessageComponentInterface {
         $this->clients->attach($conn);
 
         echo "New connection! ({$conn->resourceId})\n";
+        // monitor the sessions 
+        $this->list_session_tokens();
         // $conn->send("resource {{$conn->resourceID}}");
     }
 
@@ -24,12 +49,15 @@ class SabreSession implements MessageComponentInterface {
         $numRecv = count($this->clients) - 1;
         echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
             , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
-
-        foreach ($this->clients as $client) {
-            if ($from !== $client) {
-                // The sender is not the receiver, send to each client connected
-                $client->send($msg);
-            }
+        
+        $response  = json_decode($msg); 
+       
+        if($response->getHotel) { 
+            /**
+             * we will fetch the hotel now 
+             * then we will see if there is any token  
+             */
+            // $this->fetch_hotel(); 
         }
     }
 
@@ -44,5 +72,14 @@ class SabreSession implements MessageComponentInterface {
         echo "An error has occurred: {$e->getMessage()}\n";
 
         $conn->close();
+    }
+
+
+    protected function list_session_tokens(){ 
+        foreach(self::$tokens as $token_key => $token) {
+            echo 'Token no.', $token_key,"\n\r"; 
+            echo "Token session : ", $token['token_access'] , "\n\r"; 
+            echo "Token count:", $token['count'] , "\n\r"; 
+        }
     }
 }
