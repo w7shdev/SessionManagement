56e88d7fa25e1b50ef7b63265078f5450f217ddf
Muhannad Alrisi <muhannad@local>
2020-08-02 15:04:55 +0400

    add funcitonality to start a session and close it before request a call to the GDS

--- src/SabreSession.php
+++ src/SabreSession.php
@@ -2,6 +2,7 @@
 namespace SessionSync;
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
+// use GuzzleHttp\Client;
 use SessionSync\SabreRestAPI; 
 
 ## TODO check the auto loading PSR-4 
@@ -16,7 +17,6 @@ class SabreSession implements MessageComponentInterface {
      */
     protected static $tokens = []; 
 
-    protected static $token_counter = 0; 
     /**
      * Sabre Object handler 
      */
@@ -26,11 +26,11 @@ class SabreSession implements MessageComponentInterface {
         $this->clients = new \SplObjectStorage;
 
         if(count(self::$tokens) < 1 ){
-            self::$token_counter++; 
+            
             $this->sabreApi = new SabreRestAPI(); 
             self::$tokens[] = [
                 'token_access' => $this->sabreApi->get_token(), 
-                'count' =>  self::$token_counter
+                'count' => 0
             ]; 
         }
     }
@@ -40,9 +40,8 @@ class SabreSession implements MessageComponentInterface {
         $this->clients->attach($conn);
 
         echo "New connection! ({$conn->resourceId})\n";
-        // monitor the sessions 
+        
         $this->list_session_tokens();
-        // $conn->send("resource {{$conn->resourceID}}");
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
@@ -53,11 +52,10 @@ class SabreSession implements MessageComponentInterface {
         $response  = json_decode($msg); 
        
         if($response->getHotel) { 
-            /**
-             * we will fetch the hotel now 
-             * then we will see if there is any token  
-             */
-            // $this->fetch_hotel(); 
+            $this->start_session(); 
+            // $response = $this->get_hotel_list(); 
+            // $this->send($response->getBody()->getContents()); 
+            $this->close_session(); 
         }
     }
 
@@ -74,7 +72,14 @@ class SabreSession implements MessageComponentInterface {
         $conn->close();
     }
 
-
+    /**
+     * Get the hotel list 
+     * @return \Object client response 
+     */
+    private function get_hotel_list(){ 
+        $this->sabreApi->set_access_token($this->token_session); 
+        // return $this->sabreApi->fetch_hotel();
+    }
     protected function list_session_tokens(){ 
         foreach(self::$tokens as $token_key => $token) {
             echo 'Token no.', $token_key,"\n\r"; 
@@ -82,4 +87,70 @@ class SabreSession implements MessageComponentInterface {
             echo "Token count:", $token['count'] , "\n\r"; 
         }
     }
+
+    /**
+     * Token index 
+     */
+    private $current_session; // refer to the index of the token 
+    /**
+     *token string 
+     */
+    private $token_session;
+    /**
+     * Get the token session size 
+     *  
+     *@return \integer session token array size 
+     */
+    protected function count_token(){ 
+        return count(self::$tokens); 
+    }
+    
+    /**
+     * Set the session index of the $tokens
+     * and the session string and increment the 
+     * session token
+     * 
+     * @return \void 
+     */
+    protected function start_session(){
+
+        if($this->count_token() == 0 ) { 
+            $this->current_session = 0; 
+        }else{ 
+            $this->current_session = $this->get_avilable_session(); 
+        }
+
+        //we can do this 
+        $this->token_session = self::$tokens[$this->current_session]['token_access']; 
+        self::$tokens[$this->current_session]['count']++; 
+    }
+
+    /**
+     * Empty session holder remove the counter 
+     * so another client can use the same token 
+     * 
+     * @return \void 
+     */
+    protected function close_session(){ 
+        self::$tokens[$this->current_session]['count']--;
+        $this->current_session = null; 
+        $this->token_session = null;  
+    }
+
+    /**
+     * get the session that the counter of it 
+     * is under 50 
+     */
+    protected function get_avilable_session(){ 
+        $session_index = 0; 
+        foreach(self::$tokens as $index => $token) { 
+            if($token['count'] < 50)  { 
+                $session_index = $index; 
+            break; 
+            }
+        }
+
+        return $session_index; 
+    }
+
 }
